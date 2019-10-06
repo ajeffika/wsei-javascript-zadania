391922d04ba21eccaf8553f6ee3f51d3548df678
ajeffika <jan.mistarz6767@gmail.com>
2019-10-06 12:41:12 +0200

    created task 02

--- 1_Zadania/obowiązkowe/2_Tablice/js/app.js
+++ 1_Zadania/obowiązkowe/2_Tablice/js/app.js
@@ -4,20 +4,41 @@ function distFromAverage(arr) {
   for (var i = 0; i < arr.length; i++) {
     sum = sum + arr[i]
   }
-  let avg = sum/arr.length;
+  let avg = sum / arr.length;
   arr.forEach(el => {
     newTab.push(el - avg);
   });
   return newTab
 }
-let arr = [1,2,3,4,5,6,7];
+
+let arr = [1, 2, 3, 4, 5, 6, 7];
 distFromAverage(arr);
 
 // Zadanie 1
 
-var favouriteFruits = ['apple', 'pear','grape','banana'];
-console.log(favouriteFruits[favouriteFruits.length-1]);
+var favouriteFruits = ['apple', 'pear', 'grape', 'banana'];
+console.log(favouriteFruits[favouriteFruits.length - 1]);
 
-for(var i=0; i<favouriteFruits.length; i++){
+for (var i = 0; i < favouriteFruits.length; i++) {
   console.log(favouriteFruits[i])
 }
+
+//zadanie 2
+
+/!!!!!!!!!!!!!!!
+
+
+//zadanie 3
+
+
+function createArray(number) {
+  var newArray = [];
+  for (var counter = 1; counter <= number; counter++) {
+    newArray.push(counter);
+  }
+  return newArray;
+}
+console.log(createArray(6));
+console.log(createArray(1));
+console.log(createArray(-6));
+console.log(createArray(0));
